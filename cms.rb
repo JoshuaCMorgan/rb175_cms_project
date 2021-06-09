9692f6ba5d0fd8be33817ab0fd21555886edd0a5
Joshua Morgan <jmorg0605@gmail.com>
2021-06-09 11:31:10 -0400

    refactor code for stability

--- cms.rb
+++ cms.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'pp'
 
 configure do
   enable :sessions
@@ -17,26 +18,27 @@ end
 
 # Display document titles
 get "/" do
+  puts 'Request headers:'
+  puts request.env.class
+  pp request.env
+  'User agent: ' + request.env['HTTP_USER_AGENT']
   erb(:index)
 end
 
-def error_for_filename(filename)
-  return "#{filename} does not exist" unless @files.include?(filename)
-  false
+def valid_filename?(filename)
+  @files.include?(filename)
 end
 
 # View contents of document
 get "/:filename" do 
-  filename = params[:filename].strip
+  file_path = @root + "/data/" + params[:filename]
   
-  error = error_for_filename(filename)
-  if error
-    session[:error] = error
-    redirect "/"
-  else
-    file_path = @root + "/data/" + filename
+  if File.file?(file_path)
     headers["Content-Type"] = "text/plain"
     File.read(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exit."
+    redirect "/"
   end
 end
 
